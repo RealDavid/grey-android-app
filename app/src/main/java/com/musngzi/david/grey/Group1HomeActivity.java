0b0b0cab4292b0e44cd11aaefa9da05eda7797e1
David <musinguzi16@itu.edu.tr>
2019-01-31 16:10:26 +0300

    start

---
+++ app/src/main/java/com/musngzi/david/grey/Group1HomeActivity.java
@@ -0,0 +1,13 @@
+package com.musngzi.david.grey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Group1HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group1_home);
+    }
+}
